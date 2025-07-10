812c535aa5b80dc65da9c99b7dbd157be4672bc7
Pronoy Banik <pronoybanik82@gmail.com>
2025-07-10 21:40:03 +0600

    solve error

--- src/errors/handleValidationError.ts
+++ src/errors/handleValidationError.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { TErrorSources, TGenericErrorResponse } from '../app/interface/error';
+import { TErrorSources, TGenericErrorResponse } from '../interface/error';
 
 const handleValidationError = (
   err: mongoose.Error.ValidationError,
