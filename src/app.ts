62e8d81e88fc2c578e36b917e4dbeeef22262062
Pronoy Banik <pronoybanik82@gmail.com>
2025-07-10 22:26:28 +0600

    add url

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', "https://job-app-frontend-rho.vercel.app"], credentials: true }));
 app.use(cookieParser());
 
 // application routes
